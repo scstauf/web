abfa0a52d9ee142a3d6daa787a87df30ab87bfa3
Scott Christopher Stauffer <scstauf@gmail.com>
2015-03-20 08:07:07 -0500

    BrowserDetector object
    Provided an object called BrowserDetector with getBrowserName().

--- browser_detector.js
+++ browser_detector.js
@@ -4,21 +4,23 @@
  * only works for IE, Firefox, Opera, Chrome, and Safari.
  */
 
-function getBrowserName() {
-	var browserName = '';
-	
-	if (typeof window.clipboardData != 'undefined')
-		browserName = 'ie';
-	else if (typeof MozPowerManager != 'undefined')
-		browserName = 'moz';
-	else if (typeof opr != 'undefined')
-		browserName = 'opera';
-	else if (typeof chrome != 'undefined')
-		browserName = 'chrome';
-	else
-		browserName = 'safari';
-	
-	return browserName;
+var BrowserDetector = {
+    getBrowserName: function() {
+        var browserName = '';
+
+        if (typeof window.clipboardData != 'undefined')
+            browserName = 'ie';
+        else if (typeof MozPowerManager != 'undefined')
+            browserName = 'moz';
+        else if (typeof opr != 'undefined')
+            browserName = 'opera';
+        else if (typeof chrome != 'undefined')
+            browserName = 'chrome';
+        else
+            browserName = 'safari';
+
+        return browserName;
+    }
 }
 
 // a1f6ed979ad81d7b3ed45a5d64bd2c6f
