a78925eed16db554c4c0deec269f532ccd50d1ad
Scott Christopher Stauffer <scstauf@gmail.com>
2015-03-20 14:43:41 -0500

    Added new functionality
    locked is a flag that stops the Mouse from recording position.
    getCoordinates unlocks the Mouse and returns its current coordinates.

--- trap.js
+++ trap.js
@@ -11,6 +11,7 @@ if (typeof jQuery != 'undefined') {
             x: 0, y: 0
         },
         isDown: false,
+        locked: false,
         waitForSelect: false,
         reset: function () {
             this.down.x = 0;
@@ -18,11 +19,19 @@ if (typeof jQuery != 'undefined') {
             this.up.x = 0;
             this.up.y = 0;
             this.isDown = false;
+        },
+        getCoordinates: function () {
+            this.locked = false;
+            return {
+                x1: this.down.x, y1: this.down.y,
+                x2: this.up.x, y2: this.up.y
+            };
         }
     };
 
     $(document).ready(function () {
         $('body').mousedown(function (e) {
+            if (Mouse.locked) return;
             if (e.which == 1 && window.getSelection().toString().length == 0) {
                 if (!Mouse.isDown) {
                     Mouse.down.x = e.pageX;
@@ -33,12 +42,14 @@ if (typeof jQuery != 'undefined') {
         });
 
         $('body').mouseup(function (e) {
+            if (Mouse.locked) return;
             if (e.which == 1) {
                 if (Mouse.isDown) {
                     if ((Mouse.waitForSelect && window.getSelection().toString().length > 0) || !Mouse.waitForSelect) {
                         Mouse.up.x = e.pageX;
                         Mouse.up.y = e.pageY;
                         Mouse.isDown = false;
+                        Mouse.locked = true;
                     } else {
                         Mouse.reset();
                     }
