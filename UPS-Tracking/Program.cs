ea5eea00b98a08bbd29ccf784d0d5e66c0ec6463
Scott Christopher Stauffer <scstauf@gmail.com>
2015-10-08 13:25:32 -0500

    Update Program.cs

--- UPS-Tracking/Program.cs
+++ UPS-Tracking/Program.cs
@@ -8,25 +8,73 @@ namespace UPSTracker
 {
     class Program
     {
+        enum TrackerMethod
+        {
+            Html,
+            Ws,
+            Xml
+        }
+
+        static string TrimNullBytes(string input)
+        {
+            var output = new StringBuilder();
+
+            foreach (char c in input.ToCharArray())
+                if (c != '\0')
+                    output.Append(c);
+
+            return output.ToString();
+        }
+
         static void Main(string[] args)
         {
-            //var sql = "SELECT TOP 15 TrackingNumber FROM UPSShipments WHERE PlacedDate BETWEEN '20150801' AND '20150901'";
+            //// Extrapolate data from UPS XML responses
+            //var outputPath = @"D:\output.txt";
+            //var trackingDetails = new List<Helpers.TrackingXmlParser.TrackingDetail>();
+            //var parser = new Helpers.TrackingXmlParser();
+            //foreach (var xml in System.IO.File.ReadAllLines(outputPath))
+            //    trackingDetails.Add(parser.Extrapolate(xml, string.Empty));
 
-            //foreach (System.Data.DataRow row in Helpers.DataHelper.GetDT(sql).Rows)
+            //// Get XML data from UPS
+            //var method = TrackerMethod.Xml;
+            //var trackingNumbersPath = @"D:\tracking_numbers.txt";
+            //var service = new UPSTracker.Helpers.TrackingServiceHelper();
+            //var trackingNumbers = System.IO.File.ReadAllLines(trackingNumbersPath);
+            //int i = 0, end = trackingNumbers.Length;
+            //foreach (var trackingNumber in trackingNumbers)
+            //{
+            //    i++;
+            //    Console.Write("[{0}/{1}]: ", i, end);
+            //    var response = string.Empty;
+            //    switch (method)
+            //    {
+            //        case TrackerMethod.Html:
+            //            response = UPSTracker.Helpers.UPSTracker.RequestTrackingData(trackingNumber).ToString();
+            //            break;
+            //        case TrackerMethod.Ws:
+            //            response = service.GetDeliveryInformation(trackingNumber);
+            //            break;
+            //        case TrackerMethod.Xml:
+            //            response = XmlServiceClient.RequestTrackingDetails(trackingNumber);
+            //            break;
+            //    }
+
+            //    System.IO.File.AppendAllText(outputPath, TrimNullBytes(response));
+            //}
+
+            //var startDate = DateTime.Now.AddMonths(-1);
+            //var endDate = DateTime.Now;
+            //var trackingNumbers = Helpers.DataHelper.GetTrackingNumbers(startDate, endDate);
+
+            //foreach (System.Data.DataRow row in trackingNumbers.Rows)
             //{
             //    var trackingNumber = row["TrackingNumber"].ToString();
             //    var trackingData = UPSTracker.Helpers.UPSTracker.RequestTrackingData(trackingNumber);
             //    Console.WriteLine(trackingData.ToString());
             //}
 
-            // foreach (var trackingNumber in goodTrackingNumbers)
-            // {
-                // var trackingData = Helpers.UPSTracker.RequestTrackingData(trackingNumber);
-                // Console.WriteLine(trackingData.ToString());
-            // }
-
-            // Console.WriteLine("Done processing!");
-            // Console.ReadLine();
+            Console.WriteLine("Done processing!");
+            Console.ReadLine();
         }
     }
 }
