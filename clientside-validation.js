ad583c911330a2ecf966cd32681e1b994c1f62e5
Scott Christopher Stauffer <scstauf@gmail.com>
2016-05-11 14:16:22 -0500

    Update clientside-validation.js

--- clientside-validation.js
+++ clientside-validation.js
@@ -1,22 +1,141 @@
-function validate() {
-    /* Scotty ate javascript for lunch today. */
+/*
+ 
+ minimalist client-side form validation
 
-    var spans = $('.field-validation-valid');
-    var fields = $('*[required="required"]');
+ mark fields for validation by giving them a v-msg attribute,
+ use a span to display the message on failure.
+
+ specify validation constraints using attributes like:
+
+	<input type="text" name="zipCode" v-msg="Please enter a valid zip code." minlength="5" isnumeric="true" />
+	<span v-target="zipCode" v-label></span>
+
+	<input type="text" name="emailAddr" v-msg="Please enter a valid email address." isemailaddress="true" />
+	<span v-target="emailAddr" v-label></span>
+
+	<input type="text" name="phoneNum" v-msg="Please enter a valid phone number." isphonenumber="true" />
+	<span v-target="phoneNum" v-label></span>
+
+ */
+
+function isValidEmailAddress(emailAddress) {
+    if (emailAddress.trim().length === 0) {
+        return false;
+    }
+
+    var atSymbols = 0, periods = 0, i = 0, len = emailAddress.length;
+
+    if (emailAddress[len - 1] === '.') {
+        return false;
+    }
+
+    for (i = 0; i < len; i++) {
+        switch (emailAddress[i]) {
+            case '@':
+                if (atSymbols > 1) {
+                    return false;
+                }
+
+                atSymbols++;
+                break;
+
+            case '.':
+                if (atSymbols === 0) {
+                    return false;
+                }
+
+                periods++;
+                break;
+        }   
+    }
+
+    return atSymbols === 1 && periods > 0;
+}
+
+function isValidPhoneNumber(phoneNumber) {
+    var digits = 0;
+
+    for (var i = 0; i < phoneNumber.length; i++) {
+        if (!isNaN(phoneNumber[i]) && phoneNumber[i] !== ' ') {
+            digits++;
+        }
+    }
+
+    return digits >= 10;
+}
+
+function validate() {
+    /*
+    Scotty ate javascript for lunch today.
+    */
+
+    var spans = $('*[v-label]');
+    var fields = $('*[v-msg]');
     var name = '',
         field = '',
         validationMessage = '',
         fieldValue = '';
 
+    var scrollTo = null;
+
+    var passed = true;
+
     for (var i = 0; i < spans.length; i++) {
-        name = $(spans[i]).attr('data-valmsg-for');
-        field = $(fields).filter('[name="{0}"]'.interp(name));
+        name = $(spans[i]).attr('v-target');
+        field = $(fields).filter('[name="'+ name + '"]');
         fieldValue = $(field).val();
-        validationMessage = $(field).attr('validationmessage');
-        
-        // custom validation goes here
-        if (fieldValue.length === 0) {
+        validationMessage = $(field).attr('v-msg');
+
+        var minLength = $(field).attr('minlength');
+        var isEmailAddress = $(field).attr('isemailaddress');
+        var isPhoneNumber = $(field).attr('isphonenumber');
+        var isNumeric = $(field).attr('isnumeric');
+        var isInvalid = false;
+
+		if (fieldValue.trim() === '') {
+			isInvalid = true;
+		}
+	
+        if (!isInvalid && minLength !== undefined) {
+            isInvalid = (fieldValue.length < minLength);
+        }
+		
+        if (!isInvalid && isEmailAddress !== undefined) {
+            isInvalid = !isValidEmailAddress(fieldValue);
+        }
+		
+        if (!isInvalid && isPhoneNumber !== undefined) {
+            isInvalid = !isValidPhoneNumber(fieldValue);
+        }
+		
+        if (!isInvalid && isNumeric !== undefined) {
+            isInvalid = isNaN(fieldValue);
+        }
+
+        if (isInvalid) {
             $(spans[i]).text(validationMessage);
+            passed = false;
+
+            if (!scrollTo) {
+                scrollTo = field;
+            }
+        }
+        else {
+            $(spans[i]).text('');
         }
     }
+
+    if (!passed) {
+        $('html, body').animate({ scrollTop: $(scrollTo).offset().top - 70 }, 400);
+    }
+
+    return passed;
 }
+
+$(document).ready(function () {
+    $('form').submit(function (e) {
+        if (!validate()) {
+            e.preventDefault();
+        }
+    });
+});
