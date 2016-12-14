9d748d794e6286ce8abf5649b783872b8f63d698
Scott Christopher Stauffer <scstauf@gmail.com>
2016-12-14 16:29:09 -0600

    Update browser_detector.js

--- browser_detector.js
+++ browser_detector.js
@@ -5,27 +5,27 @@
  */
 
 var BrowserDetector = {
-	getBrowserName: function () {
-		var ua = navigator.userAgent,
-			name = 'unknown';
-		
-		if (ua.indexOf('Edge/') > -1) 
-			name = 'edge';
-		else if (ua.indexOf('Trident/') > -1)
-			name = 'ie';
-		else if (ua.indexOf('OPR/') > -1)
-			name = 'opera';
-		else if (ua.indexOf('Chrome/') > -1)
-			name = 'chrome';
-		else if (ua.indexOf('Firefox/') > -1)
-			name = 'moz';
-		else if (ua.indexOf('Safari/') > -1)
-			name = 'safari';
-		else 
-			console.error('could not determine browser from user agent');
-		
-		return name;
-	}
+    getBrowserName: function () {
+        var ua = navigator.userAgent,
+            name = 'unknown';	
+        
+        if (ua.indexOf('Edge/') > -1) 
+            name = 'edge';
+        else if (ua.indexOf('Trident/') > -1)
+            name = 'ie';
+        else if (ua.indexOf('OPR/') > -1)
+            name = 'opera';
+        else if (ua.indexOf('Chrome/') > -1)
+            name = 'chrome';
+        else if (ua.indexOf('Firefox/') > -1)
+            name = 'moz';
+        else if (ua.indexOf('Safari/') > -1)
+            name = 'safari';
+        else 
+            console.error('could not determine browser from user agent');
+
+        return name;
+    }
 };
 
 // a1f6ed979ad81d7b3ed45a5d64bd2c6f
