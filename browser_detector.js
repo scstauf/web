a36b2b3a62075efb5c3c67bf2e5739cd570736b0
Scott Christopher Stauffer <scstauf@gmail.com>
2016-12-14 16:22:45 -0600

    Update browser_detector.js
    Check the userAgent because some of the old browser specific properties were removed with browser updates.

--- browser_detector.js
+++ browser_detector.js
@@ -1,26 +1,31 @@
 /**
  * browser_dectector.js by scstauf@gmail.com
  *
- * only works for IE, Firefox, Opera, Chrome, and Safari.
+ * only works for Edge, IE, Opera, Chrome, Firefox, and Safari.
  */
 
 var BrowserDetector = {
-    getBrowserName: function() {
-        var browserName = '';
-
-        if (typeof window.clipboardData != 'undefined')
-            browserName = 'ie';
-        else if (typeof MozPowerManager != 'undefined')
-            browserName = 'moz';
-        else if (typeof opr != 'undefined')
-            browserName = 'opera';
-        else if (typeof chrome != 'undefined')
-            browserName = 'chrome';
-        else
-            browserName = 'safari';
-
-        return browserName;
-    }
-}
+	getBrowserName: function () {
+		var ua = navigator.userAgent,
+			name = 'unknown';
+		
+		if (ua.indexOf('Edge/') > -1) 
+			name = 'edge';
+		else if (ua.indexOf('Trident/') > -1)
+			name = 'ie';
+		else if (ua.indexOf('OPR/') > -1)
+			name = 'opera';
+		else if (ua.indexOf('Chrome/') > -1)
+			name = 'chrome';
+		else if (ua.indexOf('Firefox/') > -1)
+			name = 'moz';
+		else if (ua.indexOf('Safari/') > -1)
+			name = 'safari';
+		else 
+			console.error('could not determine browser from user agent');
+		
+		return name;
+	}
+};
 
 // a1f6ed979ad81d7b3ed45a5d64bd2c6f
