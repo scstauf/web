7c1b41aafb05f9d8a57ea90be6c60b406e1e8025
Scott Christopher Stauffer <scstauf@gmail.com>
2018-02-04 23:22:49 -0600

    Create pinger.bat

---
+++ pinger.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set /a failed=0
+
+call :ping google 
+call :ping yahoo 
+call :ping facebook 
+call :ping instagram
+rem call :ping microsoft
+
+echo.
+
+echo %failed% host(s) failed.
+goto :EOF
+
+:ping
+echo Pinging %1
+ping -n 1 "%1.com" > NUL
+if ERRORLEVEL 1 (set /a failed=1+failed)
